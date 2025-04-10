5f09d516df06816f2af65358027ac1a16914fd34
Hyunwoo <woo980711@gmail.com>
2025-04-10 16:43:51 +0900

    [dev] Add NVIDIA env value and GUI/OpenGL package

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 source /opt/ros/humble/setup.bash
 
 # 워크스페이스가 빌드되어 있다면 setup.bash를 source 하기 위한 조건문
-if [ -f "/ros2_ws/install/setup.bash" ]; then
-    source /ros2_ws/install/setup.bash
+if [ -f "/root/ros2_ws/install/setup.bash" ]; then
+    source /root/ros2_ws/install/setup.bash
 fi
 
 # 입력된 명령어 실행
